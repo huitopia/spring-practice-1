61d3cb5c80bc2b9c8c0a964ffcda142c8b4081e3
huitopia <huitopia27@gmail.com>
2024-05-02 09:52:40 +0900

    setting

---
+++ src/main/java/com/springprj1/SpringPrj1Application.java
@@ -0,0 +1,13 @@
+package com.springprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springprj1/SpringPrj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
