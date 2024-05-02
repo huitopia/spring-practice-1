45d6643489e41d2ab6d1934793d711a6b6cabcb1
huitopia <huitopia27@gmail.com>
2024-05-02 10:18:42 +0900

    회원가입

---
+++ src/main/java/com/springprj1/controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.springprj1.controller;
+
+import com.springprj1.domain.Member;
+import com.springprj1.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("member")
+public class MemberController {
+
+    private final MemberService service;
+
+    @GetMapping("signup")
+    public String signupForm() {
+        return "member/signup";
+    }
+
+    @PostMapping("signup")
+    public String signup(Member member) {
+        service.signup(member);
+
+        return null;
+    }
+}
